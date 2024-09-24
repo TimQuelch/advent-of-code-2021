9a05961675088a9b9c55aba2eb01796062aa22e1
Tim Quelch <tim@tquelch.com>
2024-09-24 15:10:55 +1000

    Add changes during cleanup

--- src/AdventOfCode2021.jl
+++ src/AdventOfCode2021.jl
@@ -8,7 +8,7 @@ using Reexport
 @reexport using ReTest
 
 # The days which have been solved
-days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 18, 20, 21]
+days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 18, 20, 22]
 
 # Generate lists of files and modules
 dstrs = map(d -> "d" * lpad(d, 2, '0'), days)
